eec532d7291e640743bdf4cecf3003ba0385d2ad
Tom <thomas.uren@jcu.edu.au>
2019-03-29 10:43:26 +1000

    finish intermediate exercises

---
+++ Prac_4/Week_5_Lecture.py
@@ -0,0 +1,18 @@
+########################################################################################################################
+#                           Part 1 - dictionary key and values
+########################################################################################################################
+
+# name_to_age = {"Bill":21, "Jane":4, "Jack":54}
+# user_name = input('Name: ')
+# user_age = int(input('Age: '))
+# name_to_age[user_name] = user_age
+# for name, age in name_to_age.items():
+#     print("{:10} - {:5}".format(name, age))
+#
+# print(name_to_age)
+
+# this is a dictionary comprehension
+# name_to_age = {"Bill":21, "Jane":4, "Jack":54}
+# result = {name: age for name, age in name_to_age.items() if age < 18}
+# print(result)
+
---
+++ Prac_4/list_exercises.py
@@ -0,0 +1,42 @@
+"""exercise in lists for Prac 04, CP1404"""
+########################################################################################################################
+#                           Part 1 - Basic list operations
+########################################################################################################################
+
+
+# This is the first part of the exercise
+# def main():
+#     numbers = []
+#     SIZE_OF_LIST = 5
+#     for i in range(1, SIZE_OF_LIST+1):
+#         number = int(input("Number: "))
+#         numbers.append(number)
+#
+#     average_number = sum(numbers)/len(numbers)
+#     print("The first number is {}".format(numbers[0]))
+#     print("The last number is {}".format(numbers[-1]))
+#     print("The smallest number is {}".format(min(numbers)))
+#     print("The largest number is {}".format(max(numbers)))
+#     print("The average number is {}".format(average_number))
+#
+#
+# main()
+
+
+########################################################################################################################
+#                           Part 2 - Woefully inadequate security checker
+########################################################################################################################
+
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+
+def main():
+    username = input("Enter your username: ")
+    if username in usernames:
+        print("Access Granted!")
+    else:
+        print("Access Denied!")
+
+
+main()
